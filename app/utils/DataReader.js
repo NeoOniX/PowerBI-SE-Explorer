b913ce8c125a5f1f1a71fa230b65a358996920fd
OniX <gauthier.delzenne@gmail.com>
2023-05-22 16:14:26 +0200

    Update DataReader.js
    Remove comments

--- app/utils/DataReader.js
+++ app/utils/DataReader.js
@@ -88,54 +88,6 @@ class DataReader {
                 }
             });
 
-        // // Exports
-        // const exportsDataDict = {};
-        // const e = fs.readdirSync(join(path, "/Exports/"));
-        // e.map(name => ({
-        //     name,
-        //     time: fs.statSync(join(path, "/Exports/", name)).mtime.getTime(),
-        // }))
-        //     .sort((a, b) => a.time - b.time)
-        //     .forEach(file => {
-        //         JSON.parse(fs.readFileSync(join(path, "/Exports/", file.name))).forEach(
-        //             exportData => {
-        //                 const d = new Date(file.time);
-        //                 exportsDataDict[exportData.URL] = {
-        //                     ...exportData,
-        //                     Update: `${d.getDate() < 10 ? "0" + d.getDate() : d.getDate()}/${
-        //                         d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1
-        //                     }/${d.getFullYear()} à ${d.getHours()}:${
-        //                         d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()
-        //                     }`,
-        //                 };
-        //             }
-        //         );
-        //     });
-
-        // // Anomalies
-        // const anomaliesDataDict = {};
-        // const a = fs.readdirSync(join(path, "/Anomalies/"));
-        // a.map(name => ({
-        //     name,
-        //     time: fs.statSync(join(path, "/Anomalies/", name)).mtime.getTime(),
-        // }))
-        //     .sort((a, b) => a.time - b.time)
-        //     .forEach(file => {
-        //         JSON.parse(fs.readFileSync(join(path, "/Anomalies/", file.name))).forEach(
-        //             anomalyData => {
-        //                 const d = new Date(file.time);
-        //                 anomaliesDataDict[anomalyData.URL] = {
-        //                     ...anomalyData,
-        //                     Update: `${d.getDate() < 10 ? "0" + d.getDate() : d.getDate()}/${
-        //                         d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1
-        //                     }/${d.getFullYear()} à ${d.getHours()}:${
-        //                         d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()
-        //                     }`,
-        //                 };
-        //             }
-        //         );
-        //     });
-
         return {
             Exports: Object.values(exportsDataDict),
             Anomalies: Object.values(anomaliesDataDict),
